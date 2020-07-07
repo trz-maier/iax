fa3802896aa58c4e2256f7c55d23aea21c7fc5d3
Bartosz <b.schatton@gmail.com>
2020-07-07 11:38:45 +0100

    added box_out_mask utility fun

--- iax/utilities/__init__.py
+++ iax/utilities/__init__.py
@@ -1,5 +1,5 @@
-from iax.utilities.other import progress_bar
+from iax.utilities.other import progress_bar, box_out_mask
 from iax.utilities.formatting import format_seconds, format_ordinal
 from iax.utilities.plots import plot_prediction
 
-__all__ = ['progress_bar', 'plot_prediction', 'format_seconds', 'format_ordinal']
+__all__ = ['progress_bar', 'plot_prediction', 'format_seconds', 'format_ordinal', 'box_out_mask']
--- iax/utilities/other.py
+++ iax/utilities/other.py
@@ -17,16 +17,26 @@ def progress_bar(current, maximum, length: int = 50) -> str:
     return "[%s%s] %s%%" % ('#'*l1, ' '*l2, str(progress))
 
 
-def box_out_mask(mask: np.array) -> np.array():
+def box_out_mask(mask: np.array, pixel_expand: int = 0) -> np.array:
     """
     Function boxes out the mask overs its far-most edges
     :param mask: boolean mask in form of a 2-d array
+    :param pixel_expand: value by which to expand the resulting box in each dimension (where possible)
     :return: new mask as a 2-d array
     """
     rows = np.any(mask, axis=1)
     cols = np.any(mask, axis=0)
     r_min, r_max = np.where(rows)[0][[0, -1]]
     c_min, c_max = np.where(cols)[0][[0, -1]]
+
+    r_min = max(0, r_min - pixel_expand)
+    c_min = max(0, c_min - pixel_expand)
+    r_max = min(mask.shape[0], r_max + pixel_expand)
+    c_max = min(mask.shape[1], c_max + pixel_expand)
+
+    print(r_min, r_max, c_min, c_max)
+
     out = mask.copy()
-    out[r_min:r_max+1, c_min:c_max+1] = True
+    out[r_min:r_max + 1, c_min:c_max + 1] = True
+
     return out
